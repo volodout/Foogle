4f4586a40a04e0706a3f0e2b3a25488206887c4f
volodout <volodout@gmail.com>
2024-11-11 12:01:02 +0500

    edit tests

--- tests/init_test.py
+++ tests/init_test.py
@@ -1,8 +1,8 @@
 import unittest
 from init import count_files_in_directory
 
-EMPTY_DIR = r'C:\Users\volod\PycharmProjects\Foogle\tests\empty_dir'
-QQ_DIR = r'C:\Users\volod\PycharmProjects\Foogle\tests\qq'
+EMPTY_DIR = r'empty_dir'
+QQ_DIR = r'qq'
 
 class InitTest(unittest.TestCase):
     def test_count_files_in_empty_directory(self):
