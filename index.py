8761abe2b1b20f2d258b1b4610d6c646f1823898
volodout <volodout@gmail.com>
2024-11-17 23:48:51 +0500

    output with tf-idf

--- index.py
+++ index.py
@@ -2,11 +2,11 @@ from init_index import InitIndex
 
 
 class Index:
-
     def __init__(self, directory, progress_bar):
         init = InitIndex(directory, progress_bar)
         self.data = init.get_data()
         self.phrases = init.get_phrases()
+        self.tf_idf_data = init.tf_idf_data
 
     def start(self):
         while True:
@@ -19,12 +19,14 @@ class Index:
                     print('Данная фраза не найдена')
                     continue
                 print('Найдены следующие совпадения:')
-                for phrase, directory in self.phrases[request]:
+                results = sorted(self.phrases[request], key=lambda x: x[1], reverse=True)
+                for phrase, directory in results:
                     print(f'"{phrase}" in {directory}')
             else:
-                if len(self.data[request]) == 0:
+                if len(self.tf_idf_data[request]) == 0:
                     print('Данное слово не найдено')
                     continue
                 print('Найдены следующие совпадения:')
-                for word, directory in self.data[request]:
-                    print(f'"{word}" in {directory}')
+                results = sorted(self.tf_idf_data[request], key=lambda x: x[2], reverse=True)
+                for word, directory, score in results:
+                    print(f'"{word}" in {directory} (TF-IDF: {score:.4f})')
--- init.py
+++ init.py
@@ -1,4 +1,6 @@
+import math
 import os
+from collections import defaultdict
 
 
 def count_files_in_directory(directory):
@@ -17,3 +19,23 @@ def count_files_in_directory(directory):
         return 0
     return file_count
 
+def compute_tf_idf(data, total_documents):
+    tf_idf = defaultdict(list)
+    document_word_counts = defaultdict(lambda: defaultdict(int))
+    word_document_counts = defaultdict(int)
+
+    # Подсчет TF и количества документов, содержащих слово
+    for substring, occurrences in data.items():
+        word_document_counts[substring] = len(occurrences)
+        for word, doc in occurrences:
+            document_word_counts[doc][word] += 1
+
+    # Вычисление TF-IDF
+    for substring, occurrences in data.items():
+        # Обновленная формула IDF
+        idf = math.log(1 + (total_documents / (1 + word_document_counts[substring])))
+        for word, doc in occurrences:
+            tf = document_word_counts[doc][word] / sum(document_word_counts[doc].values())
+            tf_idf[substring].append((word, doc, tf * idf))
+
+    return tf_idf
